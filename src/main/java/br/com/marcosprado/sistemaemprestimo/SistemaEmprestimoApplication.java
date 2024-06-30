9b38cc92b3de9f1bf6dd658117711e89ac1ce714
marqosPrado <marcospradofelix@gmail.com>
2024-06-30 20:46:15 -0300

    setup do projeto

---
+++ src/main/java/br/com/marcosprado/sistemaemprestimo/SistemaEmprestimoApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcosprado.sistemaemprestimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEmprestimoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaEmprestimoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/marcosprado/sistemaemprestimo/SistemaEmprestimoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcosprado.sistemaemprestimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEmprestimoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
