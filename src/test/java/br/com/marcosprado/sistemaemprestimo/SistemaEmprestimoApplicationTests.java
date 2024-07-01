94ca5130be4d9fbf5bc2fc2364242efcd073f6b6
marqosPrado <marcospradofelix@gmail.com>
2024-07-01 17:22:22 -0300

    criado testes unitários para a funcionalidade de empréstimo consignado

--- src/test/java/br/com/marcosprado/sistemaemprestimo/SistemaEmprestimoApplicationTests.java
+++ src/test/java/br/com/marcosprado/sistemaemprestimo/SistemaEmprestimoApplicationTests.java
@@ -3,7 +3,6 @@ package br.com.marcosprado.sistemaemprestimo;
 import br.com.marcosprado.sistemaemprestimo.domain.Customer;
 import br.com.marcosprado.sistemaemprestimo.domain.LoanType;
 import br.com.marcosprado.sistemaemprestimo.domain.loan.Loan;
-import br.com.marcosprado.sistemaemprestimo.domain.loan.Personal;
 import br.com.marcosprado.sistemaemprestimo.service.LoanService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 class SistemaEmprestimoApplicationTests {
@@ -67,6 +66,19 @@ class SistemaEmprestimoApplicationTests {
                 "RJ"
         );
         List<Loan> loans = loanService.checkEligibility(customer);
-        assertEquals(loans.getFirst(), null);
+        assertNull(loans.getFirst());
+    }
+
+    @Test
+    void needReturnAConsignmentLoanWhenIncomeIsGreaterThanFiveThousand() {
+        Customer customer = new Customer(
+                34,
+                "259.985.547-14",
+                "Marcos",
+                6000.01,
+                "RJ"
+        );
+        List<Loan> loans = loanService.checkEligibility(customer);
+        assertEquals(LoanType.CONSIGNMENT, loans.get(1).getLoanType());
     }
 }
