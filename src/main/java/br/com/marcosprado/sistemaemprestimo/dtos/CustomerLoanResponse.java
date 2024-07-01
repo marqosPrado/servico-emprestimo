f952e1c1c9641e1e197891c529d5f7e071a0a9a8
marqosPrado <marcospradofelix@gmail.com>
2024-07-01 17:01:22 -0300

    alteração estética

--- src/main/java/br/com/marcosprado/sistemaemprestimo/dtos/CustomerLoanResponse.java
+++ src/main/java/br/com/marcosprado/sistemaemprestimo/dtos/CustomerLoanResponse.java
@@ -1,8 +1,9 @@
 package br.com.marcosprado.sistemaemprestimo.dtos;
 
-import br.com.marcosprado.sistemaemprestimo.domain.Loan;
+import br.com.marcosprado.sistemaemprestimo.domain.loan.Loan;
 
 import java.util.List;
 
-public record CustomerLoanResponse(String name, List<Loan> loans) {
+public record CustomerLoanResponse(
+        String name, List<Loan> loans) {
 }
