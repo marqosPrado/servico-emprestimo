cdf593e0245ff587e0b22ed8c7c78790e287f1ef
marqosPrado <marcospradofelix@gmail.com>
2024-07-01 17:04:20 -0300

    criado os testes unitários para o empréstimo pessoal

--- src/test/java/br/com/marcosprado/sistemaemprestimo/SistemaEmprestimoApplicationTests.java
+++ src/test/java/br/com/marcosprado/sistemaemprestimo/SistemaEmprestimoApplicationTests.java
@@ -1,13 +1,72 @@
 package br.com.marcosprado.sistemaemprestimo;
 
+import br.com.marcosprado.sistemaemprestimo.domain.Customer;
+import br.com.marcosprado.sistemaemprestimo.domain.LoanType;
+import br.com.marcosprado.sistemaemprestimo.domain.loan.Loan;
+import br.com.marcosprado.sistemaemprestimo.domain.loan.Personal;
+import br.com.marcosprado.sistemaemprestimo.service.LoanService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SistemaEmprestimoApplicationTests {
 
+    public LoanService loanService = new LoanService();
+
     @Test
-    void contextLoads() {
+    void loanPersonalTestNeedReturnTrueWhenIncomeIsThreeThousand() {
+        Customer customer = new Customer(
+                31,
+                "259.985.547-14",
+                "Marcos",
+                3000.00,
+                "RJ"
+        );
+        List<Loan> loans = loanService.checkEligibility(customer);
+        assertEquals(LoanType.PERSONAL, loans.getFirst().getLoanType());
     }
 
+    @Test
+    void loanPersonalTestNeedReturnTrueWhenCustomerIsUnderThirtyAndLocationIsSP() {
+        Customer customer = new Customer(
+                29,
+                "259.985.547-14",
+                "Marcos",
+                3000.00,
+                "SP"
+        );
+        List<Loan> loans = loanService.checkEligibility(customer);
+        assertEquals(LoanType.PERSONAL, loans.getFirst().getLoanType());
+    }
+
+    @Test
+    void loanPersonalTestNeedReturnTrueWhenIncomeIsLessThanThreeThousand() {
+        Customer customer = new Customer(
+                31,
+                "259.985.547-14",
+                "Marcos",
+                2999.99,
+                "RJ"
+        );
+        List<Loan> loans = loanService.checkEligibility(customer);
+        assertEquals(LoanType.PERSONAL, loans.getFirst().getLoanType());
+    }
+
+    @Test
+    void loanPersonalTestNeedReturnFalseWhenIncomeIsGreaterThanFiveThousand() {
+        Customer customer = new Customer(
+                34,
+                "259.985.547-14",
+                "Marcos",
+                6000.01,
+                "RJ"
+        );
+        List<Loan> loans = loanService.checkEligibility(customer);
+        assertEquals(loans.getFirst(), null);
+    }
 }
