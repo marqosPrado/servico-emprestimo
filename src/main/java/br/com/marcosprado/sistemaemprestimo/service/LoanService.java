06edb64eeea12a883206d469e3dada4b5f374833
marqosPrado <marcospradofelix@gmail.com>
2024-07-01 17:38:50 -0300

    adicionado a instancia do empréstimo com garantia ao método 'checkEligibility'

--- src/main/java/br/com/marcosprado/sistemaemprestimo/service/LoanService.java
+++ src/main/java/br/com/marcosprado/sistemaemprestimo/service/LoanService.java
@@ -2,6 +2,7 @@ package br.com.marcosprado.sistemaemprestimo.service;
 
 import br.com.marcosprado.sistemaemprestimo.domain.Customer;
 import br.com.marcosprado.sistemaemprestimo.domain.loan.Consignment;
+import br.com.marcosprado.sistemaemprestimo.domain.loan.Guaranteed;
 import br.com.marcosprado.sistemaemprestimo.domain.loan.Loan;
 import br.com.marcosprado.sistemaemprestimo.domain.loan.Personal;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,7 @@ import java.util.List;
 public class LoanService {
 
     public List<Loan> checkEligibility(Customer customer) {
-        List<Loan> loans = List.of(new Personal(), new Consignment());
+        List<Loan> loans = List.of(new Personal(), new Consignment(), new Guaranteed());
         return loans.stream()
                 .map(loan -> loan.isEligible(customer) ? loan : null)
                 .toList();
